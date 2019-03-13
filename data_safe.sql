f6d2d86c649a81ffb1350a47dde53e5918a4a2c0
jrealt <jrealt@users.noreply.github.com>
2019-03-13 21:46:49 +0100

    fixed

--- data_safe.sql
+++ data_safe.sql
@@ -2,45 +2,54 @@ DROP database IF EXISTS data_safe;
 CREATE database IF NOT EXISTS data_safe;
 use data_safe;
 
-CREATE TABLE empleado (
-	codigo_empleado INT NOT NULL,
-    dni VARCHAR(10) NOT NULL,
-    nss VARCHAR(20) NOT NULL,
-    nombre VARCHAR (20) NOT NULL,
-    apellido VARCHAR (20) NOT NULL,
-    nombre_cat INT NOT NULL,
-    central INT NULL,
-    ciudad_res INT NOT NULL,
-    PRIMARY KEY (codigo_empleado),
-    FOREIGN KEY (nombre_cat) REFERENCES categoria (id),
-    FOREIGN KEY (central) REFERENCES sindicato (id),
-    FOREIGN KEY (ciudad_res) REFERENCES ciudad (id) );
+CREATE TABLE categoria (
+	nombre_cat VARCHAR(50) NOT NULL,
+    sueldo_base FLOAT NOT NULL,
+    hora_extra FLOAT NOT NULL, PRIMARY KEY (nombre_cat) );
 
-CREATE TABLE titulo (
-	id INT NOT NULL auto_increment,
-    nombre_titulo VARCHAR (40) NOT NULL );
-
-CREATE TABLE fijo (
-	codigo_empleado INT NOT NULL,
-    antiguedad DATETIME NOT NULL,
-    PRIMARY KEY (codigo_empleado),
-    FOREIGN KEY (codigo_empleado) REFERENCES empleado(codigo_empleado) );
-
-CREATE TABLE temporal (
-	codigo_empleado INT NOT NULL,
-    fecha_inicio_cont DATETIME NOT NULL,
-    fecha_fin_cont DATETIME NOT NULL,
-    PRIMARY KEY (codigo_empleado),
-    FOREIGN KEY (codigo_empleado) REFERENCES empleado(codigo_empleado) );
+CREATE TABLE sindicato (
+	nombre_sindicato VARCHAR(30) PRIMARY KEY,
+    cuota FLOAT );
 
 CREATE TABLE ciudad (
 	nombre_ciudad VARCHAR(40) NOT NULL,
     num_habitantes INT NOT NULL,
     PRIMARY KEY (nombre_ciudad) );
 
+CREATE TABLE empleado (
+	codigo_empleado VARCHAR(50) NOT NULL,
+    dni VARCHAR(10) NOT NULL,
+    nss VARCHAR(20) NOT NULL,
+    nombre VARCHAR (20) NOT NULL,
+    apellido VARCHAR (20) NOT NULL,
+    nombre_cat VARCHAR(50) NOT NULL,
+    central VARCHAR(30) NULL,
+    ciudad_res VARCHAR(40) NOT NULL,
+    PRIMARY KEY (codigo_empleado),
+    FOREIGN KEY (nombre_cat) REFERENCES categoria (nombre_cat),
+    FOREIGN KEY (central) REFERENCES sindicato (nombre_sindicato),
+    FOREIGN KEY (ciudad_res) REFERENCES ciudad (nombre_ciudad) );
+
+CREATE TABLE titulo (
+    nombre_titulo VARCHAR (50) NOT NULL,
+    PRIMARY KEY (nombre_titulo));
+
+CREATE TABLE fijo (
+	codigo_empleado VARCHAR(50) NOT NULL,
+    antiguedad DATETIME NOT NULL,
+    PRIMARY KEY (codigo_empleado),
+    FOREIGN KEY (codigo_empleado) REFERENCES empleado(codigo_empleado) );
+
+CREATE TABLE temporal (
+	codigo_empleado VARCHAR(50) NOT NULL,
+    fecha_inicio_cont DATETIME NOT NULL,
+    fecha_fin_cont DATETIME NOT NULL,
+    PRIMARY KEY (codigo_empleado),
+    FOREIGN KEY (codigo_empleado) REFERENCES empleado(codigo_empleado) );
+
 CREATE TABLE fecha (
-	id INT NOT NULL auto_increment,
-    fecha DATETIME NOT NULL, PRIMARY KEY (id) );
+    fecha DATETIME NOT NULL,
+    PRIMARY KEY (fecha) );
 
 CREATE TABLE agencia (
 	nombre_ciudad VARCHAR(50) NOT NULL,
@@ -52,17 +61,12 @@ CREATE TABLE agencia (
 
 CREATE TABLE titulacion (
 	nombre_titulo VARCHAR(50) NOT NULL,
-    codigo_empleado INT NOT NULL,
+    codigo_empleado VARCHAR(50) NOT NULL,
     FOREIGN KEY (nombre_titulo) REFERENCES titulo (nombre_titulo),
     FOREIGN KEY (codigo_empleado) REFERENCES empleado (codigo_empleado) );
 
-CREATE TABLE categoria (
-	nombre_cat VARCHAR(50) NOT NULL,
-    sueldo_base FLOAT NOT NULL,
-    hora_extra FLOAT NOT NULL, PRIMARY KEY (nombre_cat) );
-
 CREATE TABLE traslado (
-	codigo_empleado INTEGER NOT NULL,
+	codigo_empleado VARCHAR(50) NOT NULL,
     FOREIGN KEY (codigo_empleado) REFERENCES empleado(codigo_empleado),
     fecha DATETIME NOT NULL,
     FOREIGN KEY (fecha) REFERENCES fecha(fecha),
@@ -73,19 +77,18 @@ CREATE TABLE traslado (
     PRIMARY KEY(codigo_empleado, fecha, nombre_ciudad, nombre_agencia),
     fecha_fin DATETIME NOT NULL );
 
-CREATE TABLE sindicato (
-	nombre_sindicato VARCHAR(30) PRIMARY KEY,
-    cuota FLOAT );
-
-CREATE TABLE peticion (
-	codigo_usuario VARCHAR(50) NOT NULL,
-    codigo_prestamo FLOAT NOT NULL,
-    fecha DATE NOT NULL, PRIMARY KEY (codigo_usuario, codigo_prestamo, fecha),
-    FOREIGN KEY (codigo_empleado) REFERENCES empleado(codigo_empleado),
-    FOREIGN KEY (codigo_prestamo) REFERENCES prestamo(codigo_prestamo),
-    FOREIGN KEY (fecha) REFERENCES fecha );
-
 CREATE TABLE tipoprestamo (
 	codigo_prestamo VARCHAR(50) NOT NULL,
     tipo_interes REAL NOT NULL,
-    vigencia DATE NOT NULL );
+    vigencia DATETIME NOT NULL,
+    PRIMARY KEY (codigo_prestamo) ); 
+
+CREATE TABLE peticion (
+	codigo_empleado VARCHAR(50) NOT NULL,
+    codigo_prestamo VARCHAR(50) NOT NULL,
+    fecha DATETIME NOT NULL, 
+    UNIQUE (codigo_empleado, codigo_prestamo, fecha),
+    PRIMARY KEY (codigo_empleado, codigo_prestamo, fecha),
+    FOREIGN KEY (codigo_empleado) REFERENCES fijo(codigo_empleado),
+    FOREIGN KEY (codigo_prestamo) REFERENCES tipoprestamo(codigo_prestamo),
+    FOREIGN KEY (fecha) REFERENCES fecha(fecha) );
