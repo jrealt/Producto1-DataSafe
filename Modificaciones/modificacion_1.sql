7d53967693c1f5d3d3f0f7a5e4e3d572f11b8c36
jrealt <jrealt@fp.uoc.edu>
2019-03-15 16:03:32 +0100

    id's

--- Modificaciones/modificacion_1.sql
+++ Modificaciones/modificacion_1.sql
@@ -1,13 +1,4 @@
 /* Usamos la base de datos icxp3_7 */
 USE icxp3_7;
-/* Eliminamos la clave foranea de titulacion */
-ALTER TABLE titulacion DROP FOREIGN KEY titulacion_ibfk_1;
-/* Modificamos la columna nombre_titulo de la tabla titulacion */
-ALTER TABLE titulacion MODIFY nombre_titulo VARCHAR(20) NOT NULL;
 /* Modificamos la columna nombre_titulo de la tabla titulo */
 ALTER TABLE titulo MODIFY nombre_titulo VARCHAR(20) NOT NULL;
-/* Añadimos de nuevo la clave foranea */
-ALTER TABLE titulacion
-    ADD CONSTRAINT titulacion_ibfk_1
-    FOREIGN KEY(nombre_titulo)
-    REFERENCES titulo(nombre_titulo);
--- Modificaciones/modificacion_5.sql
+++ Modificaciones/modificacion_5.sql
@@ -1,4 +1,4 @@
 /* Usamos la base de datos icxp3_7 */
 USE icxp3_7;
 
-ALTER TABLE empleado ADD COLUMN telefono_movil VARCHAR(12) UNIQUE; 
+ALTER TABLE empleado ADD COLUMN telefono_movil VARCHAR(12) UNIQUE;
