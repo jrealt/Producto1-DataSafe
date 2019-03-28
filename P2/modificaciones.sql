e2f8450fb52bb4e3a7a03cc82e9a718f4d35deb7
jrealt <jrealt@fp.uoc.edu>
2019-03-28 19:50:49 +0100

    reformat P2 folder, modificaciones

---
+++ P2/modificaciones.sql
@@ -0,0 +1,31 @@
+USE icxp3_7;
+/* a. Cambia de nombre una titulación. */
+
+UPDATE titulo SET nombre_titulo = 'E.S.O.' WHERE id = 1;
+
+
+
+
+/* b. Cambia el año de concesión de un préstamo. */
+
+
+
+
+
+
+/* c. Cambia el precio hora a la categoría base. */
+
+
+
+
+
+
+/* d. Elimina la ciudad de Valencia. */
+
+
+
+
+
+
+
+/* e. Elimina las ciudades que no tengan ningún trabajador residiendo. */
